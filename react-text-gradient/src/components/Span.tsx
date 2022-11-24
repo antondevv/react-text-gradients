9f41aa05a10950aafa40df30b792ec02b4fcddb7
Anton Franzen <anton@exploreistic.com>
2022-11-24 23:13:07 +0100

    🔧 fix: infer

--- react-text-gradient/src/components/Span.tsx
+++ react-text-gradient/src/components/Span.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { SpanProps } from "../types"
 
-export const Span = ({ children, ...rest }: SpanProps) => (
+export const Span = ({
+  children,
+  ...rest
+}: { children: React.ReactNode } & SpanProps) => (
   <span {...rest}>{children}</span>
 )
