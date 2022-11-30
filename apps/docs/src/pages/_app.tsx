880b803852e6242d1ea69b7e1cfc427a902a8672
Anton Franzen <anton@exploreistic.com>
2022-11-30 23:25:33 +0100

    📸 docs: add analytic

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import Script from "next/script"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        defer
+        data-domain="react-text-gradient.vercel.app"
+        src="https://plausible.io/js/script.js"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
