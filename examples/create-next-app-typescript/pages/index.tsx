e6348cdca69a99b2477fd554340f31311bd17e19
Anton Franzen <anton@exploreistic.com>
2022-12-01 21:45:08 +0100

    📸 docs: add correct css

--- examples/create-next-app-typescript/pages/index.tsx
+++ examples/create-next-app-typescript/pages/index.tsx
@@ -6,11 +6,11 @@ import { LinearGradient as Lg, RadialGradient as Rg } from "react-text-gradient"
 export default function Home() {
   return (
     <div className={styles.App}>
-      <h1 className="h1">
+      <h1 className={styles.h1}>
         <Lg gradient={["to left", "#17acff ,#ff68f0"]}>Hey i'm a linear!</Lg>
       </h1>
 
-      <h1 className="h1">
+      <h1 className={styles.h1}>
         <Rg
           gradient={[
             "circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%",
