3a3b716d0f5890e525d6b4ba8e8f2e047bca0110
Anton Franzen <anton@exploreistic.com>
2022-12-04 14:05:28 +0100

    wip

--- react-text-gradients/src/__tests__/LinearGradient.test.tsx
+++ react-text-gradients/src/__tests__/LinearGradient.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import { LinearGradient } from '../components'
 
