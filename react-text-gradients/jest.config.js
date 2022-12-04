18cf18da3c2562e5b8b9434f052aec28f14c8dc2
Anton Franzen <anton@exploreistic.com>
2022-12-04 14:33:16 +0100

    fix ts-jest

--- react-text-gradients/jest.config.js
+++ react-text-gradients/jest.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
